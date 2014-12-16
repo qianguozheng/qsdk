11c4ee98ae56a425b35082a15bdd6a1ddb341487
qianguozheng <guozhengqian0825@126.com>
2014-12-16 22:16:21 +0800

    Modify AR9341 WAN-LAN-LAN-LAN-LAN sequence
    mainly revert to wan lan lan lan lan port sequence to map with the hardware.

--- target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr841n-v8.c
+++ target/linux/ar71xx/files/arch/mips/ath79/mach-tl-wr841n-v8.c
@@ -1,5 +1,5 @@
 /*
- *  TP-LINK TL-WR841N/ND v8 board support
+ *  TP-LINK TL-WR841N/ND v8/TL-MR3420 v2 board support
  *
  *  Copyright (C) 2012 Gabor Juhos <juhosg@openwrt.org>
  *
@@ -8,6 +8,7 @@
  *  by the Free Software Foundation.
  */
 
+#include <linux/gpio.h>
 #include <linux/platform_device.h>
 
 #include <asm/mach-ath79/ath79.h>
@@ -18,20 +19,24 @@
 #include "dev-gpio-buttons.h"
 #include "dev-leds-gpio.h"
 #include "dev-m25p80.h"
+#include "dev-usb.h"
 #include "dev-wmac.h"
 #include "machtypes.h"
 
 #define TL_WR841NV8_GPIO_LED_WLAN	13
 #define TL_WR841NV8_GPIO_LED_QSS	15
 #define TL_WR841NV8_GPIO_LED_WAN	18
-#define TL_WR841NV8_GPIO_LED_LAN1	19
-#define TL_WR841NV8_GPIO_LED_LAN2	20
-#define TL_WR841NV8_GPIO_LED_LAN3	21
-#define TL_WR841NV8_GPIO_LED_LAN4	12
+#define TL_WR841NV8_GPIO_LED_LAN1	20
+#define TL_WR841NV8_GPIO_LED_LAN2	21
+#define TL_WR841NV8_GPIO_LED_LAN3	22
+#define TL_WR841NV8_GPIO_LED_LAN4	19 //weeds change from 12
 #define TL_WR841NV8_GPIO_LED_SYSTEM	14
 
 #define TL_WR841NV8_GPIO_BTN_RESET	17
-#define TL_WR841NV8_GPIO_SW_RFKILL	16
+#define TL_WR841NV8_GPIO_SW_RFKILL	16	/* WPS for MR3420 v2 */
+
+#define TL_MR3420V2_GPIO_LED_3G	11
+#define TL_MR3420V2_GPIO_USB_POWER	4
 
 #define TL_WR841NV8_KEYS_POLL_INTERVAL	20	/* msecs */
 #define TL_WR841NV8_KEYS_DEBOUNCE_INTERVAL (3 * TL_WR841NV8_KEYS_POLL_INTERVAL)
@@ -78,6 +83,11 @@ static struct gpio_led tl_wr841n_v8_leds_gpio[] __initdata = {
 		.name		= "tp-link:green:wlan",
 		.gpio		= TL_WR841NV8_GPIO_LED_WLAN,
 		.active_low	= 1,
+	}, {
+		/* the 3G LED is only present on the MR3420 v2 */
+		.name		= "tp-link:green:3g",
+		.gpio		= TL_MR3420V2_GPIO_LED_3G,
+		.active_low	= 1,
 	},
 };
 
@@ -99,21 +109,27 @@ static struct gpio_keys_button tl_wr841n_v8_gpio_keys[] __initdata = {
 	}
 };
 
-static void __init tl_wr841n_v8_setup(void)
+static void __init tl_ap123_setup(int swap_phy4)
 {
 	u8 *mac = (u8 *) KSEG1ADDR(0x1f01fc00);
 	u8 *ee = (u8 *) KSEG1ADDR(0x1fff1000);
 
-	ath79_register_leds_gpio(-1, ARRAY_SIZE(tl_wr841n_v8_leds_gpio),
-				 tl_wr841n_v8_leds_gpio);
+	/* Disable JTAG, enabling GPIOs 0-3 */
+	/* Configure OBS4 line, for GPIO 4*/
+	ath79_gpio_function_setup(AR934X_GPIO_FUNC_JTAG_DISABLE,
+				 AR934X_GPIO_FUNC_CLK_OBS4_EN);
 
-	ath79_register_gpio_keys_polled(1, TL_WR841NV8_KEYS_POLL_INTERVAL,
-					ARRAY_SIZE(tl_wr841n_v8_gpio_keys),
-					tl_wr841n_v8_gpio_keys);
+	/* config gpio4 as normal gpio function */
+	ath79_gpio_output_select(TL_MR3420V2_GPIO_USB_POWER,
+				 AR934X_GPIO_OUT_GPIO);
 
 	ath79_register_m25p80(&tl_wr841n_v8_flash_data);
 
-	ath79_setup_ar934x_eth_cfg(AR934X_ETH_CFG_SW_PHY_SWAP);
+	//ath79_setup_ar934x_eth_cfg(AR934X_ETH_CFG_SW_ONLY_MODE);//weeds modify at 2014-7-23
+	if (swap_phy4)
+		ath79_setup_ar934x_eth_cfg(AR934X_ETH_CFG_SW_PHY_SWAP);
+	else
+		ath79_setup_ar934x_eth_cfg(AR934X_ETH_CFG_SW_ONLY_MODE);
 
 	ath79_register_mdio(1, 0x0);
 
@@ -122,9 +138,11 @@ static void __init tl_wr841n_v8_setup(void)
 
 	/* GMAC0 is connected to the PHY0 of the internal switch */
 	ath79_switch_data.phy4_mii_en = 1;
-	ath79_switch_data.phy_poll_mask = BIT(0);
+	//ath79_switch_data.phy_poll_mask = BIT(4);
+	ath79_switch_data.phy_poll_mask = swap_phy4 ? BIT(0) : BIT(4);
 	ath79_eth0_data.phy_if_mode = PHY_INTERFACE_MODE_MII;
-	ath79_eth0_data.phy_mask = BIT(0);
+	//ath79_eth0_data.phy_mask = BIT(4);
+	ath79_eth0_data.phy_mask = swap_phy4 ? BIT(0) : BIT(4);
 	ath79_eth0_data.mii_bus_dev = &ath79_mdio1_device.dev;
 	ath79_register_eth(0);
 
@@ -135,5 +153,24 @@ static void __init tl_wr841n_v8_setup(void)
 	ath79_register_wmac(ee, mac);
 }
 
+static void __init tl_wr841n_v8_setup(void)
+{
+	tl_ap123_setup(0);
+
+	ath79_register_leds_gpio(-1, ARRAY_SIZE(tl_wr841n_v8_leds_gpio) - 1,
+				 tl_wr841n_v8_leds_gpio);
+
+	ath79_register_gpio_keys_polled(1, TL_WR841NV8_KEYS_POLL_INTERVAL,
+					ARRAY_SIZE(tl_wr841n_v8_gpio_keys),
+					tl_wr841n_v8_gpio_keys);
+
+	gpio_request_one(TL_MR3420V2_GPIO_USB_POWER,
+			 GPIOF_OUT_INIT_HIGH | GPIOF_EXPORT_DIR_FIXED,
+			 "USB power");
+
+	ath79_register_usb();
+}
+
 MIPS_MACHINE(ATH79_MACH_TL_WR841N_V8, "TL-WR841N-v8", "TP-LINK TL-WR841N/ND v8",
 	     tl_wr841n_v8_setup);
+
